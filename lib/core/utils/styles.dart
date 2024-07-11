27ef13b819dde6e57256bc31d4684b032ba2ef61
Hussein Issa <www.hussein2001@gmail.com>
2024-07-11 20:16:01 +0300

    create app styles

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 
 abstract class Styles {
-  static const titleMedium = TextStyle(
+  static const textSyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+  static const textSyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static const textSyle18 = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w600,
+  );
+  static const textSyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+  );
+  static const textSyle30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.normal,
   );
 }
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -22,7 +22,7 @@ class HomeViewBody extends StatelessWidget {
           ),
           Text(
             'Best Seller',
-            style: Styles.titleMedium,
+            style: Styles.textSyle18,
           ),
           BestSellerListViewItem(),
         ],
