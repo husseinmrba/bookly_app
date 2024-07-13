ffaff65dbe6e7de2b57fc2a959e02ece76db6a2e
Hussein Issa <www.hussein2001@gmail.com>
2024-07-13 14:38:41 +0300

    newset books cubit done

--- lib/features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
+++ lib/features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  final HomeRepo _homeRepo;
+  NewsetBooksCubit(this._homeRepo) : super(NewsetBooksInitial());
+
+  Future<void> fetchNewsetBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await _homeRepo.fetchNewsetBooks();
+    result.fold((failure) {
+      emit(NewsetBooksFailure(failure.errMsg));
+    }, (books) {
+      emit(NewsetBooksSuccess(books));
+    });
+  }
 }
